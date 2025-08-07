01cfc8a82dff54d31f34c4063ed31417bd334deb
bhavyasindhujad <bhavyasindhu735@gmail.com>
2025-08-07 23:52:44 +0530

    initial commit

---
+++ diamond.py
@@ -0,0 +1,13 @@
+n=5
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for k in range(2*i+1):
+        print("*",end=" ")
+    print()
+for i in range(n):    
+    for j in range(i+1):
+        print(" ",end=" ")
+    for k in range(n-i*2+2):
+        print ("*",end=" ")
+    print() 
