d998f2bcb26c9b4817cc547bca515862d4164d21
bhavyasindhujad <bhavyasindhu735@gmail.com>
2025-08-08 15:09:30 +0530

    adding code

---
+++ hallow_pyramid.py
@@ -0,0 +1,10 @@
+n=5
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(i*2+1):
+        if j==0 or j==i*2 or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
