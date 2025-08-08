5e8e85f982b6e3c3aeee5b8e30a1febf911a6743
bhavyasindhujad <bhavyasindhu735@gmail.com>
2025-08-08 12:07:56 +0530

    adding code

---
+++ ben10.py
@@ -0,0 +1,13 @@
+n=5
+for i in range(n-1):
+    for j in range(i):
+        print(" ",end=" ")
+    for k in range(n-i*2+4):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for k in range(2*i+1):
+        print("*",end=" ")
+    print()
