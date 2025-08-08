ba1f5ec11f6e32a897ac40875937b33ac81d679a
bhavyasindhujad <bhavyasindhu735@gmail.com>
2025-08-08 15:07:49 +0530

    adding code

---
+++ hallowdiamond.py
@@ -0,0 +1,19 @@
+n=5
+for i in range(n-1):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(i*2+1):
+        if j==0 or j==i*2 or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+for i in range(n-1,-1,-1):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(i*2+1):
+        if j==0 or j==i*2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print() 
